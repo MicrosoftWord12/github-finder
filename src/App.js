9b7d787c068bda566b40483604069556f86112bd
MicrosoftWord12 <mrjacobbrooks@gmail.com>
2021-02-22 21:17:17 +0000

    Lecture 4 Ending

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Navbar from './components/Layout/Navbar';
 import React, { Component } from 'react';
 
 class App extends Component {
 	render() {
-		const name = 'Jacob';
-		const loading = false;
-		const showName = true;
-
-		return <div className='App'>{loading ? <h4>loading</h4> : <h1>Hello {showName && name}</h1>}</div>;
+		return (
+			<div className='App'>
+				<Navbar />
+			</div>
+		);
 	}
 }
 
